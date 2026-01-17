47f2a3987e4064dadc906eec5025cea2862aa37e
Hoscar <oscarodriguez.05.18@gmail.com>
2026-01-17 16:59:50 -0400

    components gallery

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-function App() {
+//Parte 1
+/* function App() {
     return <h1>Hello, World </h1>
 }
+ */
+
+// Parte 2
+
+function Profile() {
+    return (
+        <img
+            src="https://th.bing.com/th/id/R.23e3f025682e1dff281c9a66741d6695?rik=ofJoN06JmSK%2bBg&pid=ImgRaw&r=0"
+            alt="Michael Jackson"
+        />
+    );
+};
+
+
+export default function Gallery() {
+    return (
+        <section>
+            <h1> Personajes Famosos</h1>
+            <Profile />
+            <Profile />
+        </section>
+    )
+}
+
+
 
 const root = ReactDom.createRoot(
     document.getElementById("root")
 )
 
 root.render(
-    <App />
-)
+    <React.StrictMode>
+        <Gallery />
+    </React.StrictMode>
+);
