b7078283575bb6e72c5a0fea3c5c5113402e9c90
Hoscar <oscarodriguez.05.18@gmail.com>
2026-01-24 15:42:04 -0400

    minor fixes

--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,4 @@
-export default function Profile() {
+export function Profile() {
     return (
         <img
             src="https://th.bing.com/th/id/R.23e3f025682e1dff281c9a66741d6695?rik=ofJoN06JmSK%2bBg&pid=ImgRaw&r=0"
